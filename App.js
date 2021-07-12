30e11ee83dad415651463a4e459b35294f8513df
kentilove095 <6210110616@psu.ac.th>
2021-07-12 09:26:35 +0700

    Passing Props

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
 
 export default function App() {
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Weather zipCode="90110"/>
       <StatusBar style="auto" />
     </View>
   );
