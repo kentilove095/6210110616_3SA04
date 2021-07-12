325124727f6085eac42d5e16032583848f3a3b81
kentilove095 <6210110616@psu.ac.th>
2021-07-12 11:31:30 +0700

    CHALLENGE

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function Forecast(props){
     return(
         <View>
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <Text style={styles.fonts}>{props.main}</Text>
+            <Text style={styles.des}>{props.description}</Text>
+            <Text style={styles.tempt}>{props.temp}°C</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    fonts: {
+        width: 400,
+        textAlignVertical: 'center',
+        textAlign: 'center',
+        fontSize: 30,
+        color: "gray",
+        backgroundColor: 'orange',
+        opacity: 0.5
+    },
+    
+    des: {
+        textAlignVertical: 'center',
+        textAlign: 'center',
+        paddingTop: 15,
+        fontSize: 20,
+        color: "black",
+        backgroundColor: 'orange',
+        opacity: 0.5
+    },
+    tempt: {
+        textAlignVertical: 'center',
+        fontSize: 20,
+        textAlign: 'center',
+        height: 60,
+        backgroundColor: 'orange',
+        opacity: 0.5,        
+    }
+})
--- components/Weather.js
+++ components/Weather.js
@@ -5,16 +5,15 @@ import Forecast from './Forecast';
 
 export default function Weather(props){
     const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
+        main: 'main',
+        description: 'description',
         temp: 0
     })
 
     return(
         <ImageBackground source={require('../backg.jpg')} style ={styles.backdrop }>
-            <Text>Zip Code</Text>
-            <Text>{props.zipCode}</Text>
-        <Forecast {...forecastInfo}/>
+            <Text style={styles.fonts}>Zip Code is {props.zipCode}</Text>
+            <Forecast  {...forecastInfo}/>
         </ImageBackground>
     )
 }
@@ -22,7 +21,19 @@ export default function Weather(props){
 const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
+        flexDirection: 'column',
         width: '100%',
-        height: '100%'
+        height: '100%',
+    },
+    fonts: {
+        textAlignVertical: 'center',
+        paddingTop: 25,
+        height: 70,
+        color: 'gray',
+        fontSize: 20,
+        width: '100%',
+        textAlign: 'center',
+        backgroundColor: 'orange',
+        opacity: 0.5
     }
 })
