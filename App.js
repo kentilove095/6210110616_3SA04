5f201df4e0084af1fdf4507761af41806f673856
kentilove095 <6210110616@psu.ac.th>
2021-07-12 10:33:01 +0700

    Components and Image Background

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Weather from './components/Weather';
+import Constants from 'expo-constants';
 
-export default function App() {
- 
+export default function App() { 
   return (
     <View style={styles.container}>
-      <Weather zipCode="90110"/>
+      <Weather zipCode="90110"/> 
       <StatusBar style="auto" />
     </View>
   );
@@ -15,9 +15,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
   },
 });
